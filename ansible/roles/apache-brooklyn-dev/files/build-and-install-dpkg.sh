5d948e1512c06c852bf13105834d78217f607542
Richard Downer <richard@apache.org>
2020-02-12 21:31:22 +0000

    Fixes for Amazon Linux 2

--- ansible/roles/apache-brooklyn-dev/files/build-and-install-dpkg.sh
+++ ansible/roles/apache-brooklyn-dev/files/build-and-install-dpkg.sh
@@ -3,7 +3,7 @@
 dpkg_version=1.18.25
 
 workdir="$( mktemp --directory )"
-if [[ -z "${workdir}" -o '!' -d "${workdir}" ]]; then
+if [ -z "${workdir}" -o '!' -d "${workdir}" ]; then
     echo >&2 could not create temporary directory
     exit 1
 fi
@@ -17,7 +17,7 @@ set -e
 cd "${workdir}"
 wget http://ftp.debian.org/debian/pool/main/d/dpkg/dpkg_${dpkg_version}.tar.xz
 tar xJf dpkg_${dpkg_version}.tar.xz
-cd dpkg_${dpkg_version}
+cd dpkg-${dpkg_version}
 ./configure
 make
 make install
